cc4205c7070b05a229c767900c96098ba2ce4fdc
Alisherov Ariet <ariet.alisherov@gmail.com>
2025-06-20 20:20:39 +0600

    latest-product-style settings

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,22 @@ import Container from "@/components/container/Container";
 import "./page.css";
 import FeaturedProduct from "@/components/featured-product/FeaturedProduct";
 import featuredProductData from "@/data/featured-product.json";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="homepage">
       <Container>
         <section className="homepage-featured">
           <FeaturedProduct product={featuredProductData} />
+        </section>
+        <section className="homepage-latest-products">
+          <div className="homepage-latest-products-header">
+            <h2>Shop The Latest</h2>
+
+            <Link className="lates-products-view-all" href="/shop">
+              View All
+            </Link>
+          </div>
         </section>
       </Container>
     </main>
